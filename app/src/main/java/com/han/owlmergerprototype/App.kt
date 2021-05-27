4cc798d6b2ccc2e8cc6ce30d2788bd156832e4fb
hankim <elements0f5tyl3@gmail.com>
2021-05-27 09:27:05 +0900

    Update block double click & portrait mode

--- app/src/main/java/com/han/owlmergerprototype/App.kt
+++ app/src/main/java/com/han/owlmergerprototype/App.kt
@@ -1,6 +1,10 @@
 package com.han.owlmergerprototype
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import com.kakao.sdk.common.KakaoSdk
 
 class App: Application() {
@@ -12,7 +16,24 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+        setAllActivitySettings() // 수직모드!
         KakaoSdk.init(this, "c99b7c499a9d8e3ba072e874edbcd52e")
     }
 
+    private fun setAllActivitySettings() {
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            @SuppressLint("SourceLockedOrientationActivity")
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+
+                activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {}
+            override fun onActivityPaused(activity: Activity) {}
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {}
+        })
+    }
 }
